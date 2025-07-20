f3811271bce5215dcb011e7c332f446c48974339
David Y. Stephenson <david@davidystephenson.com>
2025-07-20 10:57:36 -0400

    Add home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import GuestList from './GuestList'
 import Settings from './Settings'
+import Home from './Home'
 
 export default function App () {
   const [guestList, setGuestList] = useState(['Zelda', 'Dorothy', 'Tallulah'])
@@ -14,12 +15,19 @@ export default function App () {
     <BrowserRouter>
       <h1>Dog Party App</h1>
       <div>
+        <Link to='/'>Home</Link>
+      </div>
+      <div>
         <Link to='/guest-list'>Guest List</Link>
       </div>
       <div>
         <Link to='/settings'>Settings</Link>
       </div>
       <Routes>
+        <Route
+          path='/'
+          element={<Home guestList={guestList} uninvite={uninvite} />}
+        />
         <Route
           path='/guest-list'
           element={<GuestList guestList={guestList} />}
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import GuestList from "./GuestList";
+import Settings from "./Settings";
+
+export default function Home (props) {
+  return (
+    <>
+      <GuestList guestList={props.guestList} />
+      <Settings guestList={props.guestList} uninvite={props.uninvite} />
+    </>
+  )
+}
